2045d20a04fdde7d26994d01ffce1a1b0bc10aa9
MarvinRo <marvins.rocha@gmail.com>
2024-09-12 16:56:26 -0400

    first commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import '../style/index.css'
+
+function App() {
+  return (
+    <div className='bg-black '>
+      <div >
+        <h1 className='text-white'>helow world</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
